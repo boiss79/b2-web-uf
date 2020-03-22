563f1326a66c570c593edeed44dcde0cae70ac3d
tlnkorr <lenaourthomas@gmail.com>
2020-03-22 13:24:49 +0100

    implement roles and permissions with laravel spatie permissions

--- src/database/seeds/UserTableSeeder.php
+++ src/database/seeds/UserTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class UserTableSeeder extends Seeder
 {
@@ -9,8 +12,34 @@ class UserTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         factory(App\User::class, 30)->create();
+        
+        $adminUser = User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'birthday' => $faker->dateTime($max = '-10 years', $timezone = null),
+            'description' => $faker->paragraph,
+            'url_picture' => 'https://place-hold.it/300',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $adminUser->assignRole('admin');
+
+        $moderatorUser = User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'moderator@moderator.com',
+            'email_verified_at' => now(),
+            'birthday' => $faker->dateTime($max = '-10 years', $timezone = null),
+            'description' => $faker->paragraph,
+            'url_picture' => 'https://place-hold.it/300',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        $moderatorUser->assignRole('admin');
     }
 }
--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -2,5 +2,10 @@
     <div class="container text-center">
         <p class="mb-3">&copy; 2020 &bull; Ma Fiche de Révision</p>
         <p>Made with ♥ at Bordeaux</p>
+        @can('access administation')
+            <p class="mt-3">
+                <a href="{{ route('test') }}">Administration</a>
+            </p>
+        @endcan
     </div>
 </footer>
